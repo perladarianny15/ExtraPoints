15bb9b7b35908175dce883454bdfecd1bf3180c8
perladarianny15 <perlad0015@gmail.com>
2019-08-22 23:17:53 -0400

    secondcommit

---
+++ App01.Android/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 1f8fe8c447cc336e652e536d1090e70bfa51fc70 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1491
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
---
+++ App01/LayaoutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App01
+{
+    public partial class LayaoutPage : ContentPage
+    {
+        public LayaoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
